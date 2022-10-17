ad69b7f2d47948209fdb6a4ac50582df8d564070
Pham Quang Phuc <phuc31082002@gmail.com>
2022-10-17 22:09:25 +0700

    MainScreen

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from "react";
+import { StyleSheet, Text, useColorScheme } from "react-native";
+import MainScreen from "./source/MainScreen.js";
+
+const App = () => {
+  return <MainScreen />;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
